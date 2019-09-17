b1538cf3df49cf5436a2b6535a059620a60a575d
NEETLee <996870835@qq.com>
2019-09-17 18:29:27 +0800

    初始化codemirror组件

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="app">
+        <codemirror :value="input" :options="cmOption"
+                    @cursorActivity="onCmCursorActivity"
+                    @ready="onCmReady"
+                    @focus="onCmFocus"
+                    @blur="onCmBlur"></codemirror>
+    </div>
+</template>
+
+<script>
+    import {codemirror} from 'vue-codemirror'
+    import formatter from "vkbeautify"
+    import highlight from 'vue-highlight.js'
+    import 'codemirror/lib/codemirror.css'
+    import 'codemirror/mode/xml/xml.js'
+    import 'codemirror/theme/base16-light.css'
+    import 'codemirror/addon/selection/active-line.js'
+    import 'codemirror/addon/edit/closetag.js'
+
+    export default {
+        name: 'formatter',
+        data() {
+            return {
+                input: "asdfsadtasdr",
+                cmOption: {
+                    tabSize: 4,
+                    styleActiveLine: true,
+                    lineNumbers: true,
+                    autoCloseTags: true,
+                    line: true,
+                    mode: 'application/xml',
+                    theme: 'base16-light',
+                    viewportMargin: true,
+                    lineWrapping: true,
+                }
+            }
+        },
+        components: {codemirror, formatter, highlight},
+        methods: {
+            onCmCursorActivity(codemirror) {
+                console.log('onCmCursorActivity', codemirror)
+            },
+            onCmReady(codemirror) {
+                console.log('onCmReady', codemirror)
+            },
+            onCmFocus(codemirror) {
+                console.log('onCmFocus', codemirror)
+            },
+            onCmBlur(codemirror) {
+                console.log('onCmBlur', codemirror)
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 95vh;
+    }
+
+    .CodeMirror {
+        height: 100%;
+    }
+
+    .CodeMirror-code {
+        font-size: 1.5rem;
+    }
+</style>
